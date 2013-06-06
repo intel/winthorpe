8140dd5cc07ec711277b28aead57c0381839b9d4
Janos Kovacs <jankovac503@gmail.com>
2013-06-06 16:42:40 +0300

    sphinx-speech-engine: permanently removing some commented out code

--- src/plugins/sphinx-speech-engine/utterance.h
+++ src/plugins/sphinx-speech-engine/utterance.h
@@ -7,28 +7,6 @@
 #define CANDIDATE_MAX        1000
 
 
-#if 0
-struct word_s {
-    const char *word;
-    int32_t start;
-    int32_t end;
-};
-
-struct candidate_s {
-    double  quality;
-    size_t  nword;
-    word_t  words[CANDIDATE_WORD_MAX + 1];
-};
-
-struct utternace_s {
-    const char *id;
-    double quality;
-    uint32_t length;
-    size_t ncand;
-    candidate_t **cands;
-};
-#endif
-
 void utterance_start(context_t *ctx);
 void utterance_end(context_t *ctx);
 
