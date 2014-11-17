0d38c603b43fa69e5bd5ba24f1eeb0232d520ee8
Amarnath Valluri <amarnath.valluri@linux.intel.com>
2014-11-17 12:45:52 +0200

    initial implementation crosswalk w3c speech extension
    Change-Id: I9ab54fa26cecf2e14df6f7f0dfbcea17270a3ebb

---
+++ src/plugins/client-api/w3c-speech/crosswalk-extension/speech_instance.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SPEECH_SPEECH_INSTANCE_H_
+#define SPEECH_SPEECH_INSTANCE_H_
+
+#include <glib.h>
+#include <thread>  // NOLINT
+#include <queue>
+#include <string>
+
+#include "common/extension.h"
+#include "common/picojson.h"
+
+class SpeechInstance : public common::Instance {
+ public:
+  SpeechInstance();
+  virtual ~SpeechInstance();
+
+ protected:
+  static gboolean IOWatchCb(GIOChannel* source,
+                            GIOCondition condition,
+                            gpointer data);
+  static gboolean ProcessPendingRequestsCb(gpointer data);
+  static gboolean ProcessPendingRepliesCb(gpointer data);
+
+ private:
+  // common::Instance implementation.
+  virtual void HandleMessage(const char* msg);
+  virtual void HandleSyncMessage(const char* message);
+
+  void QueueReply(const std::string& reply);
+  void QueueRequest(const std::string& req);
+  bool SendRequest(const char* message);
+  uint32_t ReadReply(char** reply);
+
+  std::queue<std::string> pending_replies_;
+  std::queue<std::string> pending_requests_;
+  int fd_;
+  GIOChannel *channel_;
+  guint watcher_id_;
+  guint pending_request_timer_;
+  guint pending_reply_timer_;
+};
+#endif  // SPEECH_SPEECH_INSTANCE_H_
